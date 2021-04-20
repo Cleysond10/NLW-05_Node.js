e0ae5e9231338327ec411e3af824a773e23654e7
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-04-19 21:07:22 -0300

    Create server

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { request, response } from 'express'
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: "Olá, Mundo!"
+  })
+});
+
+app.post("/", (request, response) => {
+  return response.json({
+    message: "Usuário salvo."
+  })
+});
+
+app.listen(3333, () => {
+  console.log("Server is Running")
+});
